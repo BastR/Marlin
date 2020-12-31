c5f437e4b620ad4de7925edbd7ef5b24f4ac7984
Bastien R <bastien.rossi@icloud.com>
2020-12-31 12:24:31 +0100

    Update pins_BTT_SKR_PRO_common.h

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
@@ -228,12 +228,15 @@
 // Heaters / Fans
 //
 #define HEATER_0_PIN                        PB1   // Heater0
-#define HEATER_1_PIN                        PD14  // Heater1
-#define HEATER_2_PIN                        PB0   // Heater1
+//#define HEATER_1_PIN                        PD14  // Heater1
+//#define HEATER_2_PIN                        PB0   // Heater1
 #define HEATER_BED_PIN                      PD12  // Hotbed
-#define FAN_PIN                             PC8   // Fan0
-#define FAN1_PIN                            PE5   // Fan1
-#define FAN2_PIN                            PE6
+//#define FAN_PIN                             PC8   // Fan0
+//#define FAN1_PIN                            PE5   // Fan1
+//#define FAN2_PIN                            PE6
+
+#define FAN_PIN                             PD14   // Fan0 //Part fan
+#define FAN1_PIN                            PB0   // Fan1  //Hotend fan
 
 #ifndef E0_AUTO_FAN_PIN
   #define E0_AUTO_FAN_PIN               FAN1_PIN
