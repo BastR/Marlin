d06b18a6aa6eb90ba0e1cd532388d964e0e558fa
Bastien R <bastien.rossi@icloud.com>
2021-03-14 13:55:41 +0100

    up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -739,7 +739,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.15, 80.13, 400, 411.15 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.10, 80.10, 400, 411.15 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -984,7 +984,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 45, -2.70 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 45, -2.50 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
